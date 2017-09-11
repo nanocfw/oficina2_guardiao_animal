668310c206c88bd4c314a487a453a3ea3bafa5cc
fernandobd42 <fernandolucasbd@gmail.com>
2017-09-11 14:11:34 -0300

    Adicionando target no maps Adicionando uma função target para buscar o maps na página após teclar enter no input de busca

--- frontend/GAClient/src/main/webapp/assets/js/aux_.js
+++ frontend/GAClient/src/main/webapp/assets/js/aux_.js
@@ -6,3 +6,14 @@ $(function () {
     });
     $('.modal').modal();
 });
+
+
+function enter(event) {
+    let key = event.key;
+    if (key === "Enter" && ($('#location')[0].innerHTML) !== "" )  {
+        var $doc = $('html, body');
+        $doc.animate({
+            scrollTop: $($.attr($('.scroll')[0], 'href')).offset().top
+        }, 500);
+    }
+}
