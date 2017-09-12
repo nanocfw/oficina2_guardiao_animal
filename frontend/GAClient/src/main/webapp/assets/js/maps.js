a86dca9734a2d800ccc169074ea40afdee0ddd2a
fernandobd42 <fernandolucasbd@gmail.com>
2017-09-12 18:41:20 -0300

    Adicionando onmouseover nos markPoints

--- frontend/GAClient/src/main/webapp/assets/js/maps.js
+++ frontend/GAClient/src/main/webapp/assets/js/maps.js
@@ -1,5 +1,8 @@
+var map;
+var bounceTimer;
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -25.7464549, lng: -53.06157339},
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -10,8 +13,7 @@ function initMap() {
     autocomplete.bindTo('bounds', map);
     var infowindow = new google.maps.InfoWindow();
     var marker = new google.maps.Marker({
-        map: map,
-        anchorPoint: new google.maps.Point(0, -29)
+        map: map
     });
     autocomplete.addListener('place_changed', function () {
         infowindow.close();
@@ -21,6 +23,7 @@ function initMap() {
             window.alert("Autocomplete's returned place contains no geometry");
             return;
         }
+
         // If the place has a geometry, then present it on a map.
         if (place.geometry.viewport) {
             map.fitBounds(place.geometry.viewport);
@@ -28,26 +31,7 @@ function initMap() {
             map.setCenter(place.geometry.location);
             map.setZoom(17);
         }
-        marker.setIcon(({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-        }));
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
-        var address = '';
-        if (place.address_components) {
-            address = [
-                (place.address_components[0] && place.address_components[0].short_name || ''),
-                (place.address_components[1] && place.address_components[1].short_name || ''),
-                (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-        }
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br/></div>');
-        infowindow.open(map, marker);
-        //Location details
+
         for (var i = 0; i < place.address_components.length; i++) {
             if (place.address_components[i].types[0] === 'postal_code') {
                 document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
@@ -59,9 +43,10 @@ function initMap() {
         document.getElementById('location').innerHTML = place.formatted_address;
         document.getElementById('lat').innerHTML = place.geometry.location.lat();
         document.getElementById('lon').innerHTML = place.geometry.location.lng();
+
     });
 
-    function carregarPontos() {
+    function carregarPontos(icon = 'assets/images/mark.png') {
 
         $.getJSON('assets/auxiliar/aa.json', function (pontos) {
 
@@ -69,18 +54,28 @@ function initMap() {
 
                 var marker = new google.maps.Marker({
                     position: new google.maps.LatLng(ponto.Latitude, ponto.Longitude),
-                    title: "Meu ponto personalizado! ID: " + ponto.Id.toString(),
+                    // title: "Meu ponto personalizado! ID: " + ponto.Id.toString(),
                     map: map,
-                    icon: 'assets/images/mark1.png'
+                    draggable: false,
+                    optimized: false,
+                    icon: icon
                 });
+
                 var infowindow = new google.maps.InfoWindow(), marker;
 
-                google.maps.event.addListener(marker, 'click', (function (marker) {
-                    return function () {
-                        infowindow.setContent('<p class="markMaps">' + ponto.Id + '<br/>' + ponto.nome + '<br/>' + ponto.cidade + '</p>');
-                        infowindow.open(map, marker);
-                    };
-                })(marker));
+                google.maps.event.addListener(marker, 'mouseover', function () {
+
+                    infowindow.setContent(`<p class="markMaps"> ID: ${ponto.Id} - ${ponto.nome} <br/> R$${ponto.valor}/Diaria <br/> 
+                        ${ponto.cidade} </p>`);
+                    infowindow.open(map, marker);
+
+                    marker.setIcon("assets/images/mark1.png")
+
+                });
+                google.maps.event.addListener(marker, 'mouseout', function () {
+                    infowindow.close(map, marker);
+                    marker.setIcon(icon);
+                });
             });
         });
     }
