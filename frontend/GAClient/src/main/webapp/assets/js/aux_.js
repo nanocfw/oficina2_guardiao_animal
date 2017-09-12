d77d7d8a9d9c4f7b7a1eec906edd223e0183ef02
fernandobd42 <fernandolucasbd@gmail.com>
2017-09-12 10:39:31 -0300

    Refatorando telas, Adicionando slider e scroll transformando telas components, adicionando slider na página inicial e adicionando scroll na exibição dos guardiões para melhorar a usabilidade

--- frontend/GAClient/src/main/webapp/assets/js/aux_.js
+++ frontend/GAClient/src/main/webapp/assets/js/aux_.js
@@ -4,13 +4,20 @@ $(function () {
     $(".button-collapse").sideNav({
         closeOnClick: true
     });
+    $('.slider').slider({
+        full_width: true,
+        interval: 3000,
+        transition: 1000,
+        indicators: false,
+        height: 100+'vh'
+    });
     $('.modal').modal();
 });
 
 
 function enter(event) {
     let key = event.key;
-    if (key === "Enter" && ($('#location')[0].innerHTML) !== "" )  {
+    if (key === "Enter" && ($('#location')[0].innerHTML) !== "") {
         var $doc = $('html, body');
         $doc.animate({
             scrollTop: $($.attr($('.scroll')[0], 'href')).offset().top
@@ -18,11 +25,11 @@ function enter(event) {
     }
 }
 
-$(document).ready(function() {
- 
-  // Fakes the loading setting a timeout
-    setTimeout(function() {
+$(document).ready(function () {
+
+    // Fakes the loading setting a timeout
+    setTimeout(function () {
         $('body').addClass('loaded');
     }, 2500);
- 
+
 });
