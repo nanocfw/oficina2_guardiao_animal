bf5cd0f134d6567aabe5df22f660e407aef8b2d0
fernandobd42 <fernandolucasbd@gmail.com>
2017-10-03 19:30:06 -0300

    ajustando/melhorando cadastros

--- frontend/GAClient/src/main/java/br/com/ga/client/beans/PersonBean.java
+++ frontend/GAClient/src/main/java/br/com/ga/client/beans/PersonBean.java
@@ -9,6 +9,7 @@ import br.com.ga.exceptions.InvalidEntity;
 import br.com.ga.exceptions.EntityNotFound;
 import br.com.ga.entity.Person;
 import br.com.ga.service.intf.IPersonService;
+import java.text.SimpleDateFormat;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -53,12 +54,12 @@ public class PersonBean
         try
         {
             personService.createUpdate(this.currentPerson);
-            url = extContext.encodeActionURL(ctx.getApplication().getViewHandler().getActionURL(ctx, "/main.xhtml"));
+            url = extContext.encodeActionURL(ctx.getApplication().getViewHandler().getActionURL(ctx, "/index.xhtml#login"));
             extContext.redirect(url);
             return "#login";
         } catch (InvalidEntity e)
         {
-            FacesContext.getCurrentInstance().addMessage("form:register", new FacesMessage("Esta email ja está sendo usado"));
+            FacesContext.getCurrentInstance().addMessage("form:register", new FacesMessage("Este Email ja está sendo usado, por favor digite outro Email"));
             return "cadastroInvalido";
         } catch (Exception e)
         {
@@ -95,6 +96,33 @@ public class PersonBean
         {
             ctx.addMessage("lform:login", new FacesMessage("Email ou senha está incorreto"));
             return "error";
+        } catch (Exception e)
+        {
+            return "erro";
+        }
+    }
+    
+    public String updateGuardiao()
+    {
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        ExternalContext extContext = ctx.getExternalContext();
+        String url;
+        
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/mm/yyyy");
+        String formattedDate = formatter.format(getCurrentPerson().getBirthDate());
+        System.out.println("aaaaa" + formattedDate);
+
+                
+        try
+        {
+            personService.createUpdate(this.currentPerson);
+            url = extContext.encodeActionURL(ctx.getApplication().getViewHandler().getActionURL(ctx, "/endRegister.xhtml"));
+            extContext.redirect(url);
+            return "endregister.xhtml";
+        } catch (InvalidEntity e)
+        {
+            FacesContext.getCurrentInstance().addMessage("gform:register", new FacesMessage("Erro, na funçao updateGuardiao em personBean"));
+            return "cadastroInvalido";
         } catch (Exception e)
         {
             return "erro";
