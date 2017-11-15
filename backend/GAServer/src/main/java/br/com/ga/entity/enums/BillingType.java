30308583263fdcee763529b7c3acef8975ec3a1c
Marciano da Rocha <nanocfw@gmail.com>
2017-11-15 19:51:07 -0200

    Inclusão de helpers nos enumerados.

--- backend/GAServer/src/main/java/br/com/ga/entity/enums/BillingType.java
+++ backend/GAServer/src/main/java/br/com/ga/entity/enums/BillingType.java
@@ -2,4 +2,30 @@ package br.com.ga.entity.enums;
 
 public enum BillingType {
     PER_HOUR, PER_DAY;
+
+    @Override
+    public String toString() {
+        return asString(this);
+    }
+
+    public static String asString(BillingType billingType) {
+        switch (billingType) {
+            case PER_HOUR:
+                return "Por Hora";
+            case PER_DAY:
+                return "Por Dia";
+            default:
+                return "Inválido";
+        }
+    }
+
+    public static String[] getAll() {
+        BillingType[] billingTypes = values();
+        String[] aux = new String[billingTypes.length];
+
+        for (int i = 0; i < billingTypes.length; i++)
+            aux[i] = asString(billingTypes[i]);
+
+        return aux;
+    }
 }
--- backend/GAServer/src/main/java/br/com/ga/entity/enums/PersonType.java
+++ backend/GAServer/src/main/java/br/com/ga/entity/enums/PersonType.java
@@ -10,4 +10,30 @@ package br.com.ga.entity.enums;
  */
 public enum PersonType {
     MALE, FEMALE, UNDEFINED;
+
+    @Override
+    public String toString() {
+        return asString(this);
+    }
+
+    public static String asString(PersonType personType) {
+        switch (personType) {
+            case MALE:
+                return "Homem";
+            case FEMALE:
+                return "Mulher";
+            default:
+                return "Inválido";
+        }
+    }
+
+    public static String[] getAll() {
+        PersonType[] personTypes = values();
+        String[] aux = new String[personTypes.length];
+
+        for (int i = 0; i < personTypes.length; i++)
+            aux[i] = asString(personTypes[i]);
+
+        return aux;
+    }
 }
