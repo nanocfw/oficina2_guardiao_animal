4059b7c88d4d38713bb38c1ff6e5eabe28745c50
Marciano da Rocha <nanocfw@gmail.com>
2017-11-15 19:47:53 -0200

    Ajustado exibição do tipo do animal na lista

--- backend/GAServer/src/main/java/br/com/ga/entity/enums/AnimalSize.java
+++ backend/GAServer/src/main/java/br/com/ga/entity/enums/AnimalSize.java
@@ -9,5 +9,33 @@ package br.com.ga.entity.enums;
  * @author Marciano
  */
 public enum AnimalSize {
-    SMALL, MIDDLE, LARGE
+    SMALL, MIDDLE, LARGE;
+
+    @Override
+    public String toString() {
+        return asString(this);
+    }
+
+    public static String asString(AnimalSize animalSize) {
+        switch (animalSize) {
+            case SMALL:
+                return "Pequeno";
+            case MIDDLE:
+                return "Médio";
+            case LARGE:
+                return "Grande";
+            default:
+                return "Inválido";
+        }
+    }
+
+    public static String[] getAll() {
+        AnimalSize[] animalSizes = values();
+        String[] aux = new String[animalSizes.length];
+
+        for (int i = 0; i < animalSizes.length; i++)
+            aux[i] = asString(animalSizes[i]);
+
+        return aux;
+    }
 };
