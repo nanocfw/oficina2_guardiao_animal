10ef5e846b3e973a43bb88d987f13eb37b4c37b3
Marciano da Rocha <nanocfw@gmail.com>
2017-11-19 19:22:07 -0200

    Ajustes no projeto para permirit gerar .jar

---
+++ backend/GAServer/src/test/java/application/ControllersTest.java
@@ -0,0 +1,14 @@
+package application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class ControllersTest {
+//    @Test
+//    public void contextLoads() {
+//    }
+}
---
+++ backend/GAServer/src/test/java/controllers/AnimalControllerTest.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import br.com.ga.web.controllers.AnimalController;
+import br.com.ga.web.rest.UrlMapping;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(value = AnimalController.class, secure = false)
+public class AnimalControllerTest {
+}
