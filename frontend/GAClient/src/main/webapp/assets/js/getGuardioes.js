f3b4c2510066c3ec85b1728a2e740987dbcf196a
fernandobd42 <fernandolucasbd@gmail.com>
2017-11-19 02:43:54 -0200

    refatorando busca de guardioes

--- frontend/GAClient/src/main/webapp/assets/js/getGuardioes.js
+++ frontend/GAClient/src/main/webapp/assets/js/getGuardioes.js
@@ -1,9 +1,7 @@
-// get guardioes function
-getGuardioes = () => {
+getListadeGuardioes= () => {
     var country = window.localStorage.getItem('country');
     var cityStorage = window.localStorage.getItem('loc').split(',')[0]
     var cityFormated;
-    var guardioes = '';
 
     switch (cityStorage) {
         case (cityStorage.split(' ')[1] !== undefined):
@@ -20,7 +18,7 @@ getGuardioes = () => {
             break;
     }
 
-    $.ajax({
+    return $.ajax({
         url: "http://localhost:8090/ga/person/fetchserviceprovider/0/" + country + "/" + cityFormated + "/20/0",
         type: "GET",
         contentType: 'application/json; charset=utf-8',
@@ -32,26 +30,32 @@ getGuardioes = () => {
             alert(textStatus)
         },
         success: function (persons) {
-            console.log(persons);
-            persons.map( person =>
-            {
-                var guardiao = `
-                               <div class="card vertical size-guardiao inline-guardiao">
-                            <div class="inline-component">
-                                <img class="img-guardiao" src="assets/images/img.jpg">
-                                <div class="content-guardiao text-grey">
-                                  <h3>${person.name}</h3>
-                                  <h4>${person.city}</h4>
-                                </div>
-                            </div>
-                            <div class="valor-guardiao">
-                              <h4 class="orange-text right-align" style="margin-right: 5px; margin-top: 0">R$ 40</h4>
-                              <h6 class="grey-text right-align" style="margin-right: 5px: margin-top: 0">por noite</h6>
-                            </div>
-                        </div>`;
-                guardioes = guardioes + guardiao;
-            })
+           return persons;
         }
+    })
+}
+
+// get guardioes function
+getGuardioes = () => {
+    var guardioes = '';
+    persons = getListadeGuardioes();
+
+    persons.responseJSON.map( person => {
+        var guardiao = `
+                <div class="card vertical size-guardiao inline-guardiao">
+                    <div class="inline-component">
+                        <img class="img-guardiao" src="assets/images/img.jpg">
+                        <div class="content-guardiao text-grey">
+                          <h3>${person.name}</h3>
+                          <h4>${person.city}</h4>
+                        </div>
+                    </div>
+                    <div class="valor-guardiao">
+                      <h4 class="orange-text right-align" style="margin-right: 5px; margin-top: 0">R$ 40</h4>
+                      <h6 class="grey-text right-align" style="margin-right: 5px: margin-top: 0">por noite</h6>
+                    </div>
+                </div>`;
+        guardioes = guardioes + guardiao;
     });
     if (guardioes === '') {
         return document.querySelector('#guardioes').innerHTML = `<h4 class="text-grey center-align" style="padding: 120px 0;">Desculpe, nenhum <span class="text-orange">Guardião</span> foi encontrado nesta regiao.</h4>`;
@@ -64,60 +68,29 @@ getGuardioes = () => {
 
 // get guardioes function
 getGuardioesAuth = () => {
-    var country = window.localStorage.getItem('country');
-    var cityStorage = window.localStorage.getItem('loc').split(',')[0]
-    var cityFormated;
     var guardioes = '';
+    persons = getListadeGuardioes();
 
-    switch (cityStorage) {
-        case (cityStorage.split(' ')[1] !== undefined):
-            cityFormated = cityStorage.split(' ')[0]+'%20'+cityStorage.split(' ')[1];
-            break;
-        case (cityStorage.split(' ')[2] !== undefined):
-            cityFormated = cityStorage.split(' ')[0]+'%20'+cityStorage.split(' ')[1]+'%20'+cityStorage.split(' ')[2];
-            break;
-        case (cityStorage.split(' ')[3] !== undefined):
-            cityFormated = cityStorage.split(' ')[0]+'%20'+cityStorage.split(' ')[1]+'%20'+cityStorage.split(' ')[2]+'%20'+cityStorage.split(' ')[3];
-            break;
-        default:
-            cityFormated = cityStorage;
-            break;
-    }
-
-    $.ajax({
-        url: "http://localhost:8090/ga/person/fetchserviceprovider/0/" + country + "/"+ cityFormated + "/20/0",
-        type: "GET",
-        contentType: 'application/json; charset=utf-8',
-        async: false,
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Basic " + btoa("gaadminserver:@g4re5tp4ss#"))
-        },
-        error : function(jqXHR, textStatus, errorThrown) {
-            console.log(textStatus)
-        },
-        success: function(persons) {
-            console.log(persons)
-            persons.map( person =>
-            {
-                var guardiao = `
-                        <div class="card vertical size-guardiao inline-guardiao">
-                            <div class="inline-component">
-                                <img class="img-guardiao" src="assets/images/img.jpg">
-                                <div class="content-guardiao text-grey">
-                                  <h3>${person.name}</h3>
-                                  <h4>${person.city}</h4>
-                                </div>
-                            </div>
-                            <div class="valor-guardiao">
-                              <h4 class="orange-text right-align" style="margin-right: 5px; margin-top: 0">R$ 40</h4>
-                              <h6 class="grey-text right-align" style="margin-right: 5px: margin-top: 0">por noite</h6>
-                            </div>
-                            <div class="right-align row padding-top-bottom" style="margin-right: 5px">
-                              <a class="waves-effect waves-light btn deep-orange lighten-1" href="#contratarServico">Contratar</a>
-                            </div>
-                        </div>`;
-                guardioes = guardioes + guardiao;
-            });
+    persons.responseJSON.map( person => {
+        {
+            var guardiao = `
+                <div class="card vertical size-guardiao inline-guardiao">
+                    <div class="inline-component">
+                        <img class="img-guardiao" src="assets/images/img.jpg">
+                        <div class="content-guardiao text-grey">
+                          <h3>${person.name}</h3>
+                          <h4>${person.city}</h4>
+                        </div>
+                    </div>
+                    <div class="valor-guardiao">
+                      <h4 class="orange-text right-align" style="margin-right: 5px; margin-top: 0">R$ 40</h4>
+                      <h6 class="grey-text right-align" style="margin-right: 5px: margin-top: 0">por noite</h6>
+                    </div>
+                    <div class="right-align row padding-top-bottom" style="margin-right: 5px">
+                      <a class="waves-effect waves-light btn deep-orange lighten-1" href="#contratarServico">Contratar</a>
+                    </div>
+                </div>`;
+            guardioes = guardioes + guardiao;
         }
     });
     if (guardioes === '') {
@@ -130,64 +103,33 @@ getGuardioesAuth = () => {
 };
 
 getMarkers = (map, icon = 'assets/images/mark.png') => {
-    var country = window.localStorage.getItem('country');
-    var cityStorage = window.localStorage.getItem('loc').split(',')[0]
-    var cityFormated;
+    persons = getListadeGuardioes();
 
-    switch (cityStorage) {
-        case (cityStorage.split(' ')[1] !== undefined):
-            cityFormated = cityStorage.split(' ')[0]+'%20'+cityStorage.split(' ')[1];
-            break;
-        case (cityStorage.split(' ')[2] !== undefined):
-            cityFormated = cityStorage.split(' ')[0]+'%20'+cityStorage.split(' ')[1]+'%20'+cityStorage.split(' ')[2];
-            break;
-        case (cityStorage.split(' ')[3] !== undefined):
-            cityFormated = cityStorage.split(' ')[0]+'%20'+cityStorage.split(' ')[1]+'%20'+cityStorage.split(' ')[2]+'%20'+cityStorage.split(' ')[3];
-            break;
-        default:
-            cityFormated = cityStorage;
-            break;
-    }
+    persons.responseJSON.map( person => {
+        {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(person.latitude, person.longitude),
+                // title: "Meu ponto personalizado! ID: " + ponto.Id.toString(),
+                map: map,
+                draggable: false,
+                optimized: false,
+                icon: icon
+            });
 
+            var infowindow = new google.maps.InfoWindow(), marker;
 
-    $.ajax({
-        url: "http://localhost:8090/ga/person/fetchserviceprovider/0/" + country + "/" + cityFormated + "/20/0",
-        type: "GET",
-        contentType: 'application/json; charset=utf-8',
-        async: false,
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", "Basic " + btoa("gaadminserver:@g4re5tp4ss#"))
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(textStatus)
-        },
-        success: function (persons) {
-            persons.map( person =>
-            {
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(person.latitude, person.longitude),
-                    // title: "Meu ponto personalizado! ID: " + ponto.Id.toString(),
-                    map: map,
-                    draggable: false,
-                    optimized: false,
-                    icon: icon
-                });
+            google.maps.event.addListener(marker, 'mouseover', function () {
 
-                var infowindow = new google.maps.InfoWindow(), marker;
-
-                google.maps.event.addListener(marker, 'mouseover', function () {
-
-                    infowindow.setContent(`<p class="markMaps"> ${person.name} <br/> R$ Diaria <br/> 
+                infowindow.setContent(`<p class="markMaps"> ${person.name} <br/> R$ Diaria <br/> 
                             ${person.city} </p>`);
-                    infowindow.open(map, marker);
+                infowindow.open(map, marker);
 
-                    marker.setIcon("assets/images/mark.png");
-                });
-                google.maps.event.addListener(marker, 'mouseout', function () {
-                    infowindow.close(map, marker);
-                    marker.setIcon(icon);
-                });
+                marker.setIcon("assets/images/mark.png");
+            });
+            google.maps.event.addListener(marker, 'mouseout', function () {
+                infowindow.close(map, marker);
+                marker.setIcon(icon);
             });
         }
     });
-};1
+};
